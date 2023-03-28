5a70eab3e87417602e9850fd197b5a99318d1114
SDCinfo <121976061+SDCinfo@users.noreply.github.com>
2023-03-28 09:28:40 -0400

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+
+let apiKey = "5e164bf624o32a114c22f8b0da3t6abf";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Dallas}}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
