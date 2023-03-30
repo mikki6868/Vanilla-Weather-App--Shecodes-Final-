6b6c80a2bb6bdaeeaeea42868776604b320abee8
SDCinfo <121976061+SDCinfo@users.noreply.github.com>
2023-03-30 08:41:52 -0400

    Created loop for forecast

--- src/app.js
+++ src/app.js
@@ -20,6 +20,30 @@ function formateDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+                <div class="weather-forcast-date">${day}</div>
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forcast-temperatures">
+                  <span class="weather-forcast-temperature-max">18°</span>
+                  <span class="weather-forcast-temperature-min">12°</span>
+                </div>
+              </div>
+            `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -81,3 +105,6 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
+displayForecast();
